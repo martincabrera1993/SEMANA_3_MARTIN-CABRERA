8d507f4f895519ae152d697bfd330b4b7a90eccf
martincabrera1993 <martincm22@uniandes.edu.ec>
2026-05-30 19:16:54 -0500

    clientes.cs

---
+++ clientes.cs
@@ -0,0 +1,14 @@
+@model Cliente
+<form method="post">
+<input name="Nombre" placeholder="Nombre" />
+<input name="Apellido" placeholder="Apellido" />
+<input name="Email" placeholder="Email" />
+<input name="Telefono" placeholder="Telefono" />
+<input name="Direccion" placeholder="Direccion" />
+<input name="Ciudad" placeholder="Ciudad" />
+<input name="Pais" placeholder="Pais" />
+<input name="Fecha de Nacimiento" placeholder="Fecha de Nacimiento" />
+<input name="Estado Civil" placeholder="Estado Civil" />
+<input name="Genero" placeholder="Genero" />
+<button type="submit">Guardar</button>
+</form>
